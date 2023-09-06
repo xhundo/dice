3ea9dc4cbad82e6bd5f993ffdef6f50ceb0555a9
Kurtney Joseph <josephkurt18@gmail.com>
2023-09-05 20:29:17 -0400

    styles: app componenent styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { RollDice } from './Components/RollDice';
 class App extends Component {
   render() {
     return (
-      <div className="flex items-center w-full h-screen justify-center">
+      <div className="flex flex-col items-center w-full h-screen bg-purple-500 justify-center">
         <RollDice rolls={[1, 2, 3, 4, 5, 6]} />
       </div>
     );
